3161549900ee244c4afbff3b9874b6a2707589b4
Albert Hackman <nana20p@yahoo.com>
2022-04-22 01:32:00 -0400

    Added new code

--- examples.js
+++ examples.js
@@ -1,3 +1,14 @@
 const addNum = (a,b) => {
     return a+b;
 }
+
+//Find the first non-conservative number
+
+function oddNumberOut(arr){
+       for (let i=0; i<arr.length; i++){
+           if(arr[i+1]-arr[i]!==1){
+            return arr[i+1]
+           }
+       } 
+       return null;
+}
