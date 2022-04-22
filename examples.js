cbe7e0a9acabf4d8387a76516702da9123221500
Albert Hackman <nana20p@yahoo.com>
2022-04-22 01:45:22 -0400

    Refactored answer to work for negative numbers

--- examples.js
+++ examples.js
@@ -5,7 +5,7 @@ const addNum = (a,b) => {
 //Find the first non-conservative number
 
 function oddNumberOut(arr){
-       for (let i=0; i<arr.length; i++){
+       for (let i=0; i<arr.length-1; i++){
            if(arr[i+1]-arr[i]!==1){
             return arr[i+1]
            }
